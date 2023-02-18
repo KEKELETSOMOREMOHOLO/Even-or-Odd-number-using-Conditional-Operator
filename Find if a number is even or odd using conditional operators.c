f0df6ca3e1801870668a6aa8da2776821463718f
KEKELETSOMOREMOHOLO <118248938+KEKELETSOMOREMOHOLO@users.noreply.github.com>
2023-02-18 12:07:48 +0530

    Create Find if a number is even or odd using conditional operators.c

---
+++ Find if a number is even or odd using conditional operators.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main() {
+    int num;  
+    scanf("%d", &num);
+    (num % 2 == 0)? printf("The given number is even") : printf("The given number is odd");
+        
+    return 0;
+}
